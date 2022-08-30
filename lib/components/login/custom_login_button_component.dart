236664f39b581a418ee7240490735347eccd619b
Leandro Lenon Godoy <leandrolenon@gmail.com>
2022-08-30 16:17:24 -0300

    first commit

--- lib/components/login/custom_login_button_component.dart
+++ lib/components/login/custom_login_button_component.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import '../../controllers/login_controller.dart';
 
 class CustomLoginButtonComponent extends StatelessWidget {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:login_splash_screen/controllers/home_controller.dart';
+import 'package:login_splash_screen/repositories/home_repository_imp.dart';
 
 import '../models/post_model.dart';
-import '../repositories/home_repository_mock.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeRepositoryMock());
+  final HomeController _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
@@ -23,14 +23,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home'),
+      ),
       body: ValueListenableBuilder<List<PostModel>>(
         valueListenable: _controller.posts,
         builder: (_, list, __) {
-          return ListView.builder(
+          return ListView.separated(
             itemCount: list.length,
             itemBuilder: (_, idx) => ListTile(
+              leading: Text(list[idx].id.toString()),
               title: Text(list[idx].title),
+              trailing: const Icon(Icons.arrow_forward),
             ),
+            separatorBuilder: (_, __) => const Divider(),
           );
         },
       ),
